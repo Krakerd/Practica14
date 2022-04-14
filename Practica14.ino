0a4f6335c8d2ea641dc805e911e52521513636d6
EL3S4 <mdasjupiter@protonmail.com>
2022-04-14 19:18:50 +0200

    creation

---
+++ Practica14.ino
@@ -0,0 +1,15 @@
+int ordenar[] = {9,6,7,8,3,5,4,1,2};
+int n = sizeof(ordenar)/sizeof(ordenar[0]);
+
+void ordena(int lista[]){
+
+}
+
+void setup(){
+    Serial.begin(9600);
+    delay(10);
+}
+
+void loop(){
+
+}
