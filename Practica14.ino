855705ba0facb5707c54c8f6d5a893f63ff48a15
EL3S4 <mdasjupiter@protonmail.com>
2022-04-16 18:20:42 +0200

    prototipo

--- Practica14.ino
+++ Practica14.ino
@@ -1,8 +1,33 @@
 int ordenar[] = {9,6,7,8,3,5,4,1,2};
 int n = sizeof(ordenar)/sizeof(ordenar[0]);
 
-void ordena(int lista[]){
+void ordena(int lista[], int izq, int drch){
+    int i, j, x, tmp;
+    i=izq;
+    j=drch;
+    x=lista[(izq+drch)/2];
 
+    while(i<=j){
+        while(lista[i] < x && i < right){
+            i++;
+        }
+        while(lista[j] > x && j > left){
+            j--;
+        }
+        if( i <= j ){
+            tmp = lista[i];
+            lista[i] = lista[j];
+            lista[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+    if( j > izq ){
+        ordena(lista, izq, j);
+    }
+    if( i < drch ){
+        ordena(lista,i,drch);
+    }
 }
 
 void setup(){
@@ -11,5 +36,12 @@ void setup(){
 }
 
 void loop(){
-
+    Serial.print("Lista sin ordenar: ");
+    for(int a = 0; a < n; a++){
+        Serial.print(ordenar[a]);
+    }
+    Serial.println();
+    ordena(ordenar,0,n-1);
+    Serial.print("Lista ordenada: ");
+    for(int b = 0;)
 }
